f71a24b3ae9188042ed536bb9322016facfa8826
Xpazeman <xpazeman@gmail.com>
2020-07-01 14:05:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Random Main Menu
+This mods randomizes the time and weather in the main menu screen so it changes everytime you enter it.
+
+## Installing the mod
+* If you haven't done so already, install MelonLoader by downloading and running [MelonLoader.Installer.exe](https://github.com/HerpDerpinstine/MelonLoader/releases/latest/download/MelonLoader.Installer.exe).
+* Download the latest version of RandomMainMenu.dll from the releases page.
+* Move RandomMainMenu.dll into the Mods folder in your TLD install directory.
---
+++ RandomMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System;
+using Harmony;
+using UnityEngine;
+using MelonLoader;
+
+namespace RandomMainMenu
+{
+    internal class RandomMainMenu : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Debug.Log("[random-main-menu] Version " + Assembly.GetExecutingAssembly().GetName().Version);
+        }
+    }
+
+    [HarmonyPatch(typeof(Panel_MainMenu), "Start")]
+    internal class Panel_MainMenu_Start
+    {
+        public static void Postfix(Panel_MainMenu __instance)
+        {
+            StartSettings set = __instance.m_StartSettings;
+            set.m_RandomTimeOfDay = true;
+            set.m_RandomWeather = true;
+            __instance.m_StartSettings = set;
+        }
+    }
+}
